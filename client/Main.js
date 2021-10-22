065c8a096752e24d169d460d38e86b8176f76c67
Jason Johnston <thatwebguru@gmail.com>
2021-10-22 13:34:07 -0500

    Starting page structure design

--- client/Main.js
+++ client/Main.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import Application from './components/Application';
 import './Main.css';
 
-ReactDOM.render(<Application />, document.body);
+ReactDOM.render(<Application />, document.getElementById("render-target"));
--- client/components/Application.jsx
+++ client/components/Application.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 export default class Application extends Component {
     render() {
         return (
             <>
-                <img src="images/laugh.gif" />
+                <header className="inline-flex flex-col bg-white border-r-2 border-b-2 border-gray-400 rounded-br-3xl">
+                    <div className="flex flex-col items-center justify-center m-2">
+                        <div className="flex flex-row text-4xl font-bold"><img src="images/icon.png" className="mr-3" />GCDB</div>
+                        <div className="text-xs font-bold">Game Character Database</div>
+                    </div>
+                    <nav>
+
+                    </nav>
+                </header>
             </>
         );
     }
